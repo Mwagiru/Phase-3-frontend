9eaf6b95e678ee7ad573b206ce41b054abc1c8bd
Mwagiru <joymwagiru@gmail.com>
2022-06-29 21:04:48 +0300

    Create header in span element

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='Navbar'>
+        <span className='nav-logo'><h1>QUOTSY</h1></span>
+
+    </div>
+  )
+}
+
+export default Navbar
