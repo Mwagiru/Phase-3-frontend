277c56746e067f4be3902867ee2aa062b2bef3d5
Mwagiru <joymwagiru@gmail.com>
2022-06-29 20:45:58 +0300

    Create state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React,{ useState } from 'react';
 
 const App = () => {
- 
+  const [quotes, setQuotes] = useState("");
+  const [author, setAuthor] = useState("");
   return (
  
     <>
