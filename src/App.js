954fcee5b240a2f23afc3599cb901fe68db4d98f
Mwagiru <joymwagiru@gmail.com>
2022-06-29 20:48:12 +0300

    UseEffect hook to fetch data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import React,{ useState } from 'react';
 const App = () => {
   const [quotes, setQuotes] = useState("");
   const [author, setAuthor] = useState("");
+  
+   const quoteAPI = async () => {
+    let arrayOfQuotes = [];
+    try{
+      const data = await axios.get("http://localhost:9292/quotes");
+      arrayOfQuotes = data.data;
+    
+    
+    }catch(error){
+      console.log(error)
+    }
+    try {
+     setQuotes(arrayOfQuotes.content);
+     setAuthor(arrayOfQuotes.author);
+    }catch (error){
+      console.log(error)
+    }
+  };
+    useEffect(() => {
+      quoteAPI();
+    }, []);
   return (
  
     <>
