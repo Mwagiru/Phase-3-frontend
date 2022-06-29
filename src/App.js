9f6d2f376788406b04a3b9e0df51561d18fcd243
Mwagiru <joymwagiru@gmail.com>
2022-06-29 20:52:03 +0300

    Create divs for returning data in browser

--- src/App.js
+++ src/App.js
@@ -5,30 +5,40 @@ const App = () => {
   const [quotes, setQuotes] = useState("");
   const [author, setAuthor] = useState("");
   
-   const quoteAPI = async () => {
-    let arrayOfQuotes = [];
+   const API = async () => {
+    let quotesarr = [];
     try{
-      const data = await axios.get("http://localhost:9292/quotes");
-      arrayOfQuotes = data.data;
+      const data = await axios.get();
+      quotesarr = data.data;
     
     
     }catch(error){
       console.log(error)
     }
     try {
-     setQuotes(arrayOfQuotes.content);
-     setAuthor(arrayOfQuotes.author);
+     setQuotes(quotesarr.content);
+     setAuthor(quotesarr.author);
     }catch (error){
       console.log(error)
     }
   };
     useEffect(() => {
-      quoteAPI();
+      API();
     }, []);
   return (
- 
     <>
-     KU
+      <div className="App">
+        <div className="quotebox">
+          <div className="container">
+            <div className="quote">
+              <h2>{quotes}</h2>
+            </div>
+            <div className="quotebutton">
+              <button onClick={quoteAPI}>GIMME QOUTE</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
