48fb38a15608f470bacf5dbcc329cbddd6258f72
Mwagiru <joymwagiru@gmail.com>
2022-06-29 19:11:11 +0300

    Create home.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 import React,{ useState } from 'react';
-import Form from './Components/Form';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-  useNavigate,
-} from "react-router-dom";
+;
 
 const App = () => {
  
   return (
-
-     
-      <div className="App">
-          <Form/>
-      </div>
-  
+ 
+    <div>
+      hi
+    </div>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        HEY HEY HEY HEY!!!
+    </div>
+  )
+}
+
+export default Home
