b776fe798ebd08dd942898570fdb7d7b3f18457d
Mwagiru <joymwagiru@gmail.com>
2022-06-29 20:44:24 +0300

    Move firebase.js file Remove several components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import React,{ useState } from 'react';
-import Home from "./Components/Home"
-import Input from './Components/Input';
 
 const App = () => {
  
   return (
  
-    <div>
-      {<Home/>}
-      {<Input/>}
-    </div>
+    <>
+     KU
+    </>
   );
 }
 
